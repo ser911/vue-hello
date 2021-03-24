edd3d1e8bf0b056dc4273eeadbb3653f9633275c
Sergio <sergio.marretta@libero.it>
2021-03-24 17:49:48 +0100

    testing

--- myscript.js
+++ myscript.js
@@ -2,9 +2,24 @@ const app = new Vue(
 {
   el: "#target",
   data:{
-    message: "Hi there!"
+  message: "Hi there!",
+   name: "Sergio",
+   surname: "Marretta",
+   age: "30",
+   colorChange: "red"
+
+ },
+
+ methods: {
+  colorClass: function(){
+  if (this.colorChange == "red") {
+    this.colorChange = "blue";
+  } else {
+     this.colorChange ="red"
   }
 }
 
+ }
 
+}
 );
